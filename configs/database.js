db798226177fa5eb3189bd1a659701ef3cf83c6c
Ankit Raj <ankitraj2095@gmail.com>
2025-02-10 01:35:39 +0530

    update database.js

--- configs/database.js
+++ configs/database.js
@@ -7,6 +7,7 @@ const database = await mysql.createConnection({
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
+    port: process.env.MYSQL_PORT
 });
 
 export const databaseConnection = async () => {
